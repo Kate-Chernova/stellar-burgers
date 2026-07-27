883e65e2779c8378d81801f1ba4a1f90a0b3bd50
User <Cate31405@yandex.ru>
2026-07-28 00:25:03 +0700

    v.5.1

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,14 +1,21 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useSelector } from '../../services/store';
 import { selectUser } from '@selectors';
+import { selectIsAuthChecked } from '@selectors';
+import { Preloader } from '@ui';
 type Props = {
   onlyUnAuth?: boolean;
 };
 
 export const ProtectedRoute = ({ onlyUnAuth = false }: Props) => {
   const user = useSelector(selectUser);
+  const isAuthChecked = useSelector(selectIsAuthChecked);
   const isAuth = !!user;
   const location = useLocation();
+
+  if (!isAuthChecked) {
+    return <Preloader />;
+  }
 
   if (onlyUnAuth && isAuth) {
     const from = location.state?.from || { pathname: '/' };
